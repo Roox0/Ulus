09ead788b7709e9f4347164760fe81baf801db71
Roox0 <mertcankay29@gmail.com>
2025-04-03 20:48:01 +0300

    Update Admin.py

--- Admin.py
+++ Admin.py
@@ -44,7 +44,7 @@ def shelby():
         "12": "https://raw.githubusercontent.com/t9omas/V1-Project/refs/heads/main/ig%2C_chcker_ninjapy.py",
         "13": "https://raw.githubusercontent.com/Roox0/Ulus/refs/heads/main/facebook-pubg.py",
         "14": "https://raw.githubusercontent.com/Roox0/Ulus/refs/heads/main/bein.py"
-        "15": "",
+        "15": "https://raw.githubusercontent.com/Roox0/Ulus/refs/heads/main/blu-tv-p%C4%B1n.py",
     }
     
     if secim in baglantilar:
