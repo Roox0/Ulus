7bbdf41d1dcc77dd0c9b2a626444cf0254817b38
Roox0 <mertcankay29@gmail.com>
2025-04-03 20:24:20 +0300

    Update Admin.py

--- Admin.py
+++ Admin.py
@@ -40,7 +40,7 @@ def shelby():
         "10": "https://raw.githubusercontent.com/Roox0/Ulus/refs/heads/main/d-smart.py",
         "11": "https://raw.githubusercontent.com/Roox0/Ulus/refs/heads/main/facebook.py",
         "12": "https://raw.githubusercontent.com/t9omas/V1-Project/refs/heads/main/ig%2C_chcker_ninjapy.py",
-        "13": "",
+        "13": "https://raw.githubusercontent.com/Roox0/Ulus/refs/heads/main/facebook-pubg.py",
         "14": "https://raw.githubusercontent.com/t9omas/V1-Project/refs/heads/main/ig%2C_chcker_ninjapy.py"
     }
     
