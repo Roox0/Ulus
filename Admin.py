38a6ea8c19bfa80fd490aae3fa7a1fb2a5c8da91
Roox0 <mertcankay29@gmail.com>
2025-04-03 20:39:19 +0300

    Update Admin.py

--- Admin.py
+++ Admin.py
@@ -41,7 +41,7 @@ def shelby():
         "11": "https://raw.githubusercontent.com/Roox0/Ulus/refs/heads/main/facebook.py",
         "12": "https://raw.githubusercontent.com/t9omas/V1-Project/refs/heads/main/ig%2C_chcker_ninjapy.py",
         "13": "https://raw.githubusercontent.com/Roox0/Ulus/refs/heads/main/facebook-pubg.py",
-        "14": "https://raw.githubusercontent.com/t9omas/V1-Project/refs/heads/main/ig%2C_chcker_ninjapy.py"
+        "14": "https://raw.githubusercontent.com/Roox0/Ulus/refs/heads/main/bein.py"
     }
     
     if secim in baglantilar:
