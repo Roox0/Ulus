84cb4df1f1577ca67d795653918981b8d0f6a7ee
Roox0 <mertcankay29@gmail.com>
2025-04-03 21:13:20 +0300

    Update Admin.py

--- Admin.py
+++ Admin.py
@@ -23,6 +23,7 @@ print("\x1b[38;5;117m  13\x1b[38;5;231m - Pubg-facebook Checker   | \x1b[1;32m a
 print("\x1b[38;5;117m  14\x1b[38;5;231m - Bein Checker   | \x1b[1;32m aktif ✅")
 print("\x1b[38;5;117m  15\x1b[38;5;231m - Blu Tv Pın Silici   | \x1b[1;32m kod ✅")
 print("\x1b[38;5;117m  16\x1b[38;5;231m - Proxy Çekme   | \x1b[1;32m aktif ✅")
+print("\x1b[38;5;117m  18\x1b[38;5;231m - Proxy Checker   | \x1b[1;32m aktif ✅")
 
 
 
@@ -47,7 +48,8 @@ def shelby():
         "13": "https://raw.githubusercontent.com/Roox0/Ulus/refs/heads/main/facebook-pubg.py",
         "14": "https://raw.githubusercontent.com/Roox0/Ulus/refs/heads/main/bein.py",
         "15": "https://raw.githubusercontent.com/Roox0/Ulus/refs/heads/main/blu-tv-p%C4%B1n.py",
-        "16": "https://raw.githubusercontent.com/Roox0/Ulus/refs/heads/main/proxy-cekme.py"
+        "16": "https://raw.githubusercontent.com/Roox0/Ulus/refs/heads/main/proxy-cekme.py",
+        "17": "",
     }
     
     if secim in baglantilar:
